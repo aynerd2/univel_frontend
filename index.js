ed1bed9ab5dac37f476e595db055dccf5e0dd72f
Ayobami <olawaleayowande@gmail.com>
2024-03-28 14:22:06 +0100

    updates

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-document.addEventListener('DOMContentLoaded', function(){
-      alert("Welcome to HealthPrick")
-})
+function addition (a,b){
+
+   let c =  a + b
+
+   return c
+}
+
+
+
+function subtraction (a, b){
+
+      let c = a + b
+      return c
+}
+
+console.log(subtraction(100, 300))
+
+
+const multiplication = () =>{
+
+      
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Change content, modifies structure
+// change styling
+// Add or Remove Elements
+// Respond to event (click e.t.c)
+// Access Element (getElement)
